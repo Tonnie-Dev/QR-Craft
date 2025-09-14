1714ab0abda8efb3a510464697252d369d186839
Tonnie <vontonnie@gmail.com>
2025-09-14 20:50:58 +0300

    Add Splash Screen

--- app/src/main/java/com/tonyxlab/qrcraft/MainActivity.kt
+++ app/src/main/java/com/tonyxlab/qrcraft/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tonyxlab.qrcraft
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,12 +12,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.tonyxlab.qrcraft.ui.theme.QRCraftTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition { false }
+        }
+
         enableEdgeToEdge()
+
         setContent {
             QRCraftTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
