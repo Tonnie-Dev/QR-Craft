c9cea9aaa4108c4c083b21c5e58eca12b97aec71
Tonnie <vontonnie@gmail.com>
2025-09-16 08:19:47 +0300

    Create AppDialog.kt

---
+++ app/src/main/java/com/tonyxlab/qrcraft/presentation/core/components/AppDialog.kt
@@ -0,0 +1,66 @@
+package com.tonyxlab.qrcraft.presentation.core.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import com.tonyxlab.qrcraft.presentation.core.base.theme.ui.QRCraftTheme
+
+@Composable
+fun AppDialog(
+    onDismissRequest: () -> Unit,
+    onConfirm: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    positiveButtonText: String,
+    negativeButtonText: String? = null
+) {
+    AlertDialog(
+            title = { Text(modifier = Modifier.fillMaxWidth(), text = dialogTitle) },
+            text = { Text(text = dialogText) },
+            onDismissRequest = { onDismissRequest() },
+            confirmButton = {
+                TextButton(onClick = { onConfirm() }) {
+                    Text(text = positiveButtonText)
+                }
+            },
+            dismissButton = {
+                if (negativeButtonText?.isNotBlank() == true) {
+                    TextButton(onClick = { onDismissRequest() }) {
+                        Text(text = negativeButtonText)
+                    }
+                }
+            }
+    )
+}
+
+@PreviewLightDark
+@Composable
+private fun AppDialog_Preview() {
+    QRCraftTheme {
+        Box(
+                modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = MaterialTheme.colorScheme.background),
+
+                contentAlignment = Alignment.Center
+        ) {
+            AppDialog(
+                    onDismissRequest = {},
+                    onConfirm = {},
+                    dialogTitle = "Delete",
+                    dialogText = "Delete Note?",
+                    positiveButtonText = "OK",
+                    negativeButtonText = "Cancel"
+            )
+        }
+    }
+}
