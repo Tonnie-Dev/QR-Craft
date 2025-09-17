596383e7ebd97ba6d68de9992721aba1e9a94ec1
Tonnie <vontonnie@gmail.com>
2025-09-17 18:50:03 +0300

    Revamp color palette for theme UI

--- app/src/main/java/com/tonyxlab/qrcraft/presentation/theme/ui/Color.kt
+++ app/src/main/java/com/tonyxlab/qrcraft/presentation/theme/ui/Color.kt
@@ -2,10 +2,43 @@ package com.tonyxlab.qrcraft.presentation.theme.ui
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Core
+val Primary        = Color(0xFFEBFF69)
+val Surface        = Color(0xFFEDF2F5)
+val SurfaceHigher  = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Text on surfaces
+val OnSurface      = Color(0xFF273037)
+val OnSurfaceAlt   = Color(0xFF505F6A)
+
+// Overlays
+val Overlay        = Color(0x80000000) // #000000 @ 50%
+val OnOverlay      = Color(0xFFFFFFFF)
+
+// Links
+val Link           = Color(0xFF373F05)
+val LinkBg         = Color(0x4DEBFF69) // Primary @ 30%
+
+// Status
+val Error          = Color(0xFFF12244)
+val Success        = Color(0xFF4DDA9D)
+
+// Accent text chip
+val TextAccent     = Color(0xFF583DC5)
+val TextAccentBg   = Color(0x1A583DC5) // 10%
+
+// Contact chips
+val Contact        = Color(0xFF259570)
+val ContactBg      = Color(0x1A259570) // 10%
+
+// Geo chips
+val Geo            = Color(0xFFB51D5C)
+val GeoBg          = Color(0x1AB51D5C) // 10%
+
+// Phone chips
+val Phone          = Color(0xFFC86017)
+val PhoneBg        = Color(0x1AC86017) // 10%
+
+// WiFi chips
+val WiFi           = Color(0xFF1F44CD)
+val WiFiBg         = Color(0x1A1F44CD) // 10%
