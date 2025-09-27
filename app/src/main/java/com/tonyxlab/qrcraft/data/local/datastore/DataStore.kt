3718ab68f520800112a83ceb055482472745fb1d
Tonnie <vontonnie@gmail.com>
2025-09-27 20:05:34 +0300

    Add DataStore for camera snackbar status

---
+++ app/src/main/java/com/tonyxlab/qrcraft/data/local/datastore/DataStore.kt
@@ -0,0 +1,30 @@
+package com.tonyxlab.qrcraft.data.local.datastore
+
+import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import com.tonyxlab.qrcraft.util.Constants.QR_CRAFT_DATASTORE
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+val Context.dataStore by preferencesDataStore(name = QR_CRAFT_DATASTORE)
+
+class DataStore(private val context: Context) {
+
+    val datastore = context.dataStore
+    suspend fun setSnackbarShownStatus(isShown: Boolean) {
+        datastore.edit { prefs ->
+            prefs[CAMERA_SNACKBAR_SHOWN] = isShown
+        }
+    }
+     fun getSnackbarShownStatus(): Flow<Boolean>  {
+
+        return datastore.data.map { prefs -> prefs[CAMERA_SNACKBAR_SHOWN]?: false }
+    }
+
+
+    companion object {
+        private val CAMERA_SNACKBAR_SHOWN = booleanPreferencesKey("cam_perm_status")
+    }
+}
--- app/src/main/java/com/tonyxlab/qrcraft/util/Constants.kt
+++ app/src/main/java/com/tonyxlab/qrcraft/util/Constants.kt
@@ -2,4 +2,5 @@ package com.tonyxlab.qrcraft.util
 
 object Constants {
     const val SCREEN_REGION_OF_INTEREST_FRACTION = .75f
+    const val QR_CRAFT_DATASTORE = "Qr_Prefs"
 }
