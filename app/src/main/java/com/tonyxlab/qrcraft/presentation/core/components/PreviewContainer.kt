8c2c3a72b2d09c473ace323fd6d8e951c121a2fa
Tonnie <vontonnie@gmail.com>
2025-10-14 19:15:24 +0300

    Refactor ResultContainer -> PreviewContainer

--- app/src/main/java/com/tonyxlab/qrcraft/presentation/core/components/PreviewContainer.kt
+++ app/src/main/java/com/tonyxlab/qrcraft/presentation/core/components/PreviewContainer.kt
@@ -1,48 +1,61 @@
-package com.tonyxlab.qrcraft.presentation.screens.result.components
+package com.tonyxlab.qrcraft.presentation.core.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.Dp
 import com.tonyxlab.qrcraft.R
+import com.tonyxlab.qrcraft.data.generateQrBitmap
+import com.tonyxlab.qrcraft.domain.QrData
 import com.tonyxlab.qrcraft.domain.QrDataType
-import com.tonyxlab.qrcraft.presentation.core.components.AppButton
 import com.tonyxlab.qrcraft.presentation.core.utils.spacing
-import com.tonyxlab.qrcraft.presentation.screens.result.handling.ResultUiEvent
-import com.tonyxlab.qrcraft.presentation.screens.result.handling.ResultUiState
+import com.tonyxlab.qrcraft.presentation.screens.result.components.ExpandableText
 import com.tonyxlab.qrcraft.presentation.theme.ui.Link
 import com.tonyxlab.qrcraft.presentation.theme.ui.LinkBg
-import com.tonyxlab.qrcraft.presentation.theme.ui.QRCraftTheme
 import com.tonyxlab.qrcraft.util.ifThen
 
 @Composable
-fun ResultContainer(
-    uiState: ResultUiState,
-    onEvent: (ResultUiEvent) -> Unit,
+fun PreviewContainer(
+    qrData: QrData,
+    onShare: () -> Unit,
+    onCopy: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    val qrData = uiState.dataState.qrData
+
     val shape = MaterialTheme.shapes.large
 
     val qrBoxSize = MaterialTheme.spacing.spaceMedium * 10
     val overlapSize = qrBoxSize / 2
 
-    val qrDataType = uiState.dataState.qrData.qrDataType
+    val qrDataType = qrData.qrDataType
+
 
     Box(modifier = modifier.fillMaxWidth()) {
 
@@ -72,7 +85,7 @@ fun ResultContainer(
                 if (qrDataType == QrDataType.TEXT) {
 
                     ExpandableText(text = qrData.prettifiedData)
-                }else{
+                } else {
                     Text(
                             modifier = Modifier
                                     .ifThen(qrDataType == QrDataType.LINK)
@@ -107,7 +120,7 @@ fun ResultContainer(
                                 .fillMaxWidth()
                                 .weight(1f),
                         buttonText = stringResource(id = R.string.btn_text_share),
-                        onClick = { onEvent(ResultUiEvent.ShareContent) },
+                        onClick = onShare,
                         leadingIcon = painterResource(R.drawable.share_icon)
                 )
 
@@ -116,13 +129,13 @@ fun ResultContainer(
                                 .fillMaxWidth()
                                 .weight(1f),
                         buttonText = stringResource(id = R.string.btn_text_copy),
-                        onClick = { onEvent(ResultUiEvent.CopyContent) },
+                        onClick = onCopy,
                         leadingIcon = painterResource(R.drawable.copy_icon)
                 )
             }
         }
         QrImageTile(
-                data = uiState.dataState.qrData.rawDataValue,
+                data = qrData.rawDataValue,
                 qrBoxSizeInDp = qrBoxSize,
                 overlapSize = overlapSize,
                 shape = shape
@@ -130,22 +143,35 @@ fun ResultContainer(
     }
 }
 
-@PreviewLightDark
 @Composable
-private fun ResultContainer_Preview() {
-    QRCraftTheme {
-        Column(
-                modifier = Modifier
-                        .fillMaxSize()
-                        .background(color = MaterialTheme.colorScheme.background)
-                        .padding(top = MaterialTheme.spacing.spaceMedium * 10)
-                        .padding(horizontal = MaterialTheme.spacing.spaceMedium)
+fun BoxScope.QrImageTile(
+    data: String,
+    qrBoxSizeInDp: Dp,
+    overlapSize: Dp,
+    shape: Shape
+) {
+    val sizePx = with(LocalDensity.current) { qrBoxSizeInDp.roundToPx() }
 
-        ) {
-            ResultContainer(
-                    uiState = ResultUiState(),
-                    onEvent = {}
-            )
-        }
+    val bitmap by remember(data, qrBoxSizeInDp) {
+        mutableStateOf(generateQrBitmap(data = data, sizePx = sizePx))
+    }
+
+    Card(
+            modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .size(qrBoxSizeInDp)
+                    .offset(y = -overlapSize)
+                    .shadow(MaterialTheme.spacing.spaceSmall, shape, clip = false), shape = shape,
+            elevation = CardDefaults.cardElevation(defaultElevation = MaterialTheme.spacing.spaceSmall)
+    ) {
+
+        Image(
+                bitmap = bitmap.asImageBitmap(),
+                contentDescription = stringResource(id = R.string.cds_text_qr_code),
+                modifier = Modifier
+                        .size(qrBoxSizeInDp)
+        )
     }
 }
+
+
