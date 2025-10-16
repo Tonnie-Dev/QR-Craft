5976479f4e26015cced515c99ec555a8c5e2b0e6
Tonnie <vontonnie@gmail.com>
2025-10-16 21:24:18 +0300

    Update Entry Screen

--- app/src/main/java/com/tonyxlab/qrcraft/presentation/screens/entry/EntryScreen.kt
+++ app/src/main/java/com/tonyxlab/qrcraft/presentation/screens/entry/EntryScreen.kt
@@ -3,13 +3,17 @@ package com.tonyxlab.qrcraft.presentation.screens.entry
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tonyxlab.qrcraft.navigation.NavOperations
 import com.tonyxlab.qrcraft.presentation.core.base.BaseContentLayout
@@ -20,6 +24,9 @@ import com.tonyxlab.qrcraft.presentation.screens.entry.components.EntryTextField
 import com.tonyxlab.qrcraft.presentation.screens.entry.handling.EntryActionEvent
 import com.tonyxlab.qrcraft.presentation.screens.entry.handling.EntryUiEvent
 import com.tonyxlab.qrcraft.presentation.screens.entry.handling.EntryUiState
+import com.tonyxlab.qrcraft.presentation.theme.ui.QRCraftTheme
+import com.tonyxlab.qrcraft.util.DeviceType
+import com.tonyxlab.qrcraft.util.ifThen
 import com.tonyxlab.qrcraft.util.toTopBarString
 import org.koin.androidx.compose.koinViewModel
 
@@ -39,20 +46,23 @@ fun EntryScreen(
                         ),
                         topBarTextColor = MaterialTheme.colorScheme.onSurface,
                         iconTintColor = MaterialTheme.colorScheme.onSurface
-                ){
+                ) {
                     viewModel.onEvent(EntryUiEvent.ExitEntryScreen)
                 }
             },
 
-            actionEventHandler = {_, action -> when(action){
-                EntryActionEvent.NavigateToCreateScreen -> {
-                    navOperations.navigateToCreateScreenDestination(fromEntryScreen = true)
-                }
-            is    EntryActionEvent.NavigateToPreviewScreen -> {
+            actionEventHandler = { _, action ->
+                when (action) {
+                    EntryActionEvent.NavigateToCreateScreen -> {
+                        navOperations.navigateToCreateScreenDestination(fromEntryScreen = true)
+                    }
 
-                    navOperations.navigateToPreviewScreenDestination(jsonMapString = action.jsonMapString)
+                    is EntryActionEvent.NavigateToPreviewScreen -> {
+
+                        navOperations.navigateToPreviewScreenDestination(jsonMapString = action.jsonMapString)
+                    }
                 }
-            } },
+            },
             containerColor = MaterialTheme.colorScheme.surface,
             onBackPressed = {
                 viewModel.onEvent(EntryUiEvent.ExitEntryScreen)
@@ -60,7 +70,7 @@ fun EntryScreen(
     ) { uiState ->
 
         EntryScreenContent(
-                modifier = modifier.padding(MaterialTheme.spacing.spaceMedium),
+                modifier = modifier,
                 uiState = uiState, onEvent = viewModel::onEvent
         )
     }
@@ -73,28 +83,80 @@ fun EntryScreenContent(
     modifier: Modifier = Modifier
 ) {
 
+    val windowClass = currentWindowAdaptiveInfo().windowSizeClass
+    val deviceType = DeviceType.fromWindowSizeClass(windowClass)
+
+    val isWideDevice = when (deviceType) {
+
+        DeviceType.MOBILE_PORTRAIT -> false
+        else -> true
+    }
+    val graduatedHorizontalPadding = when (deviceType) {
+
+        DeviceType.MOBILE_PORTRAIT -> MaterialTheme.spacing.spaceMedium
+        DeviceType.TABLET_PORTRAIT -> MaterialTheme.spacing.spaceTen * 3
+        DeviceType.MOBILE_LANDSCAPE -> MaterialTheme.spacing.spaceTen * 5
+        else -> MaterialTheme.spacing.spaceTen * 10
+
+    }
+
     val fields = uiState.formFields
 
     Column(
             modifier = modifier
-                    .clip(MaterialTheme.shapes.large)
-                    .background(MaterialTheme.colorScheme.surfaceContainerHigh)
-                    .padding(MaterialTheme.spacing.spaceMedium),
-            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.spaceMedium)
+                    .padding(horizontal = graduatedHorizontalPadding)
+                    .padding(top = MaterialTheme.spacing.spaceMedium),
     ) {
+        Column(
+                modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(MaterialTheme.shapes.large)
+                        .background(MaterialTheme.colorScheme.surfaceContainerHigh)
+                        .ifThen(isWideDevice) {
+                            padding(MaterialTheme.spacing.spaceTwelve * 2)
+                        }
+                        .ifThen(!isWideDevice) {
 
-        fields.forEach { field ->
+                            padding(MaterialTheme.spacing.spaceMedium)
+                        },
+                verticalArrangement = if (isWideDevice)
+                    Arrangement.spacedBy(MaterialTheme.spacing.spaceMedium)
+                else
+                    Arrangement.spacedBy(MaterialTheme.spacing.spaceSmall)
+        ) {
 
-            EntryTextField(
-                    textFieldState = field.textFieldState,
-                    placeholderString = field.placeHolder,
+            fields.forEach { field ->
+
+                EntryTextField(
+                        textFieldState = field.textFieldState,
+                        placeholderString = field.placeHolder,
+                )
+            }
+
+            EntryButton(enabled = uiState.isValidForm) {
+                onEvent(EntryUiEvent.GenerateQrCode)
+            }
+
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun EntryScreenContent_Preview() {
+
+    QRCraftTheme {
+        Column(
+                modifier = Modifier
+                        .background(MaterialTheme.colorScheme.surface)
+                        .fillMaxSize()
+                        .padding(MaterialTheme.spacing.spaceMedium)
+        ) {
+
+            EntryScreenContent(
+                    uiState = EntryUiState(),
+                    onEvent = {}
             )
         }
-
-        EntryButton(enabled = uiState.isValidForm) {
-            onEvent(EntryUiEvent.GenerateQrCode)
-        }
-
     }
-
 }
