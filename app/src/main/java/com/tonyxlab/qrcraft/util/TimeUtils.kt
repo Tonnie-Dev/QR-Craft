15b613a8df6f1c0cff51dbbc6d42fb39afc95bff
Tonnie <vontonnie@gmail.com>
2025-10-19 20:48:44 +0300

    Add TimeUtils for date formatting and conversion

---
+++ app/src/main/java/com/tonyxlab/qrcraft/util/TimeUtils.kt
@@ -0,0 +1,21 @@
+package com.tonyxlab.qrcraft.util
+
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+private val dateFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy, HH:mm")
+
+fun Long.toFormattedDate(): String {
+
+    val instant = Instant.ofEpochMilli(this)
+    val localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault())
+    return dateFormatter.format(localDateTime)
+}
+
+fun LocalDateTime.toMillis(): Long {
+    return this.atZone(ZoneId.systemDefault())
+            .toInstant()
+            .toEpochMilli()
+}
