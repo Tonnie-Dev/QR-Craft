27d03faa6dd472379eb59447a236f05713366588
Tonnie <vontonnie@gmail.com>
2025-10-19 20:48:52 +0300

    Create PlaceholderUtils.kt

---
+++ app/src/main/java/com/tonyxlab/qrcraft/util/PlaceholderUtils.kt
@@ -0,0 +1,36 @@
+package com.tonyxlab.qrcraft.util
+
+import com.tonyxlab.qrcraft.domain.model.QrData
+import com.tonyxlab.qrcraft.domain.model.QrDataType
+
+fun getRandomQrDataItem(): QrData {
+
+    val qrType = QrDataType.entries.random()
+
+    val timestamp = listOf(
+            1720870896000,
+            1755083241000,
+            1738381923000,
+            1669773603000,
+            1692593299000,
+            1746693554000
+    ).random()
+
+    return QrData(
+            displayName = generateLoremIpsum(1),
+            prettifiedData = generateLoremIpsum(26),
+            qrDataType = qrType,
+            timestamp = timestamp,
+            rawDataValue = ""
+    )
+}
+
+fun getRandomQrDataItems(count: Int = 10): List<QrData> {
+
+    return buildList {
+
+        repeat(count) {
+            add(getRandomQrDataItem())
+        }
+    }
+}
