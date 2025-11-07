0f93c5d96c913b45b87fff4ce79da7ec9de1b07c
Tonnie <vontonnie@gmail.com>
2025-11-07 17:43:36 +0300

    Set Manifest backup flag to false

--- app/src/main/java/com/tonyxlab/qrcraft/data/local/datastore/DataStore.kt
+++ app/src/main/java/com/tonyxlab/qrcraft/data/local/datastore/DataStore.kt
@@ -10,9 +10,10 @@ import kotlinx.coroutines.flow.map
 
 val Context.dataStore by preferencesDataStore(name = QR_CRAFT_DATASTORE)
 
-class DataStore(private val context: Context) {
+class DataStore(context: Context) {
 
     val datastore = context.dataStore
+
     suspend fun setSnackbarShownStatus(isShown: Boolean) {
         datastore.edit { prefs ->
             prefs[CAMERA_SNACKBAR_SHOWN] = isShown
@@ -22,7 +23,6 @@ class DataStore(private val context: Context) {
 
         return datastore.data.map { prefs -> prefs[CAMERA_SNACKBAR_SHOWN]?: false }
     }
-
 
     companion object {
         private val CAMERA_SNACKBAR_SHOWN = booleanPreferencesKey("cam_perm_status")
